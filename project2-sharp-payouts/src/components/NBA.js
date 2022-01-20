38b866ee345004dee995ed69dcae584ea1c39c56
jordanstotts <jordanstotts22@gmail.com>
2022-01-20 13:27:38 -0600

    basic layout of home page added with no styling

--- project2-sharp-payouts/src/components/NBA.js
+++ project2-sharp-payouts/src/components/NBA.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NBA = () => {
   return (
-  
+    <div></div>
   )
 };
 
--- project2-sharp-payouts/src/components/NHL.js
+++ project2-sharp-payouts/src/components/NHL.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NHL = () => {
   return (
-  
+    <div></div>
   )
 };
 
