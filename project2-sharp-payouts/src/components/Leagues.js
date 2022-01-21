9d6876ea9a409fdb9ab15cd82676bc03024fa8f8
jordanstotts <jordanstotts22@gmail.com>
2022-01-21 10:56:35 -0600

    nba component correctly fetchs api data for spreads

--- project2-sharp-payouts/src/components/Leagues.js
+++ project2-sharp-payouts/src/components/Leagues.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import NBA from './NBA';
-import NFL from './NFL';
-import MLB from './MLB';
-import NHL from './NHL';
 import { CardGroup, Card, CardImg, CardBody, CardTitle } from 'reactstrap'
-import { Link,  } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 const Leagues = () => {
--- project2-sharp-payouts/src/components/NBA.js
+++ project2-sharp-payouts/src/components/NBA.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { Nav, NavItem, NavLink, Table } from 'reactstrap'
-import { Route, Routes, Link } from 'react-router-dom'
 
 
 
 const NBA = () => { 
   const nbaLogo = require("https://blog.logomyway.com/wp-content/uploads/2017/01/nba-logo-design.jpg").default
+
+  const [nba, setNba] = useState([])
+  useEffect(() => {
+    const url = 'https://api.the-odds-api.com/v4/sports/basketball_nba/odds/?regions=us&oddsFormat=american&markets=spreads&apiKey=7016872381cf91ff6bd37f4ec388ae9b'
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          setNba(json)
+        })
+        .catch(console.error)
+  }, [])
+
+  if (!nba) {
+    return <p>Loading current odds ...</p>
+  }
+
 
   return (
     <div>
