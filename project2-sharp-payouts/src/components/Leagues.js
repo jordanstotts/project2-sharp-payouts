b12fa7d723402a0417d97e3a8259b404c6843c92
jordanstotts <jordanstotts22@gmail.com>
2022-01-25 11:55:11 -0600

    home page styling complete

--- project2-sharp-payouts/src/components/Leagues.js
+++ project2-sharp-payouts/src/components/Leagues.js
@@ -10,42 +10,42 @@ const Leagues = () => {
             <h1 className='homepage-header'>Sharp Payouts</h1>
         </header>
         <CardGroup className='leagues'>
-            <Link className='nba-card' to={ "/NBA" }>
+            <Link className='card-link' to={ "/NBA" }>
                 <div>
                 <Card className='nba'>
                     <CardImg className='homepage-logos' alt="nba logo" src="https://blog.logomyway.com/wp-content/uploads/2017/01/nba-logo-design.jpg" top width="100%"/>
-                    <CardBody>
-                        <CardTitle tag="h5">NBA</CardTitle>
+                    <CardBody className='nba-card-body'>
+                        <CardTitle className='nba-title' tag="h5">NBA</CardTitle>
                     </CardBody>
                 </Card>
                 </div>
             </Link>
-            <Link to={ "/NFL" }>
+            <Link className='card-link' to={ "/NFL" }>
             <div>
             <Card className='nfl'>
                 <CardImg className='homepage-logos' alt="nfl logo" src="https://i0.wp.com/sportytell.com/wp-content/uploads/2020/11/NFL-Team-Logos.jpg?resize=680%2C350&ssl=1" top width="100%"/>
-                <CardBody>
-                    <CardTitle tag="h5">NFL</CardTitle>
+                <CardBody className='nfl-card-body'>
+                    <CardTitle className='nfl-title' tag="h5">NFL</CardTitle>
                 </CardBody>
             </Card>
             </div>
             </Link>
-            <Link to={ "/MLB" }>
+            <Link className='card-link' to={ "/MLB" }>
             <div>
             <Card className='mlb'>
                 <CardImg className='homepage-logos' alt="mlb logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Major_League_Baseball_logo.svg/1920px-Major_League_Baseball_logo.svg.png" top width="100%"/>
-                <CardBody>
-                    <CardTitle tag="h5">MLB</CardTitle>
+                <CardBody className='mlb-card-body'>
+                    <CardTitle className='mlb-title' tag="h5">MLB</CardTitle>
                 </CardBody>
             </Card>
             </div>
             </Link>
-            <Link to={ "/NHL" }>
+            <Link className='card-link' to={ "/NHL" }>
             <div>
             <Card className='nhl'>
                 <CardImg className='homepage-logos' alt="nhl logo" src="https://1000logos.net/wp-content/uploads/2017/05/NHL-emblem-500x450.jpg" top width="100%"/>
-                <CardBody>
-                    <CardTitle tag="h5">NFL</CardTitle>
+                <CardBody className='nhl-card-body'>
+                    <CardTitle className='nhl-title' tag="h5">NFL</CardTitle>
                 </CardBody>
             </Card>
             </div>
